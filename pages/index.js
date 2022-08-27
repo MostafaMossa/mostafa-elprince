bcbc1a12bcbfe037f780157db9cefac1dd25c60b
mostafamossa <mostafaelprince65@gmail.com>
2022-08-27 23:37:17 +0200

    master

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { AppSettingsAlt, Architecture, DomainVerification, IntegrationInstructions, RemoveRedEye } from "@mui/icons-material";
+import { AppSettingsAlt, Architecture, DomainVerification, InsertEmoticon, IntegrationInstructions, RemoveRedEye } from "@mui/icons-material";
 import Image from "next/image";
 import VisitorCounter from "../components/VisitorCounter";
 
@@ -12,7 +12,7 @@ const Home = () => {
 
   return (
     <div className="gird place-items-center w-full h-full p-2 xl:p-6">
-      <div className="grid place-items-center W-0 xl:flex xl:flex-row xl:items-center xl:space-x-4 xl:w-full">
+      <div className="grid place-items-center W-0 xl:flex xl:flex-row xl:items-center xl:space-x-4 xl:w-full my-4">
         <Image src={MyPhoto} width={250} height={260} className="rounded-2xl shadow-2xl shadow-slate-800" />
         <div className="grid place-items-center w-full h-full xl:place-items-start xl:h-full xl:w-full">
           <h1 className="text-gray-400 uppercase text-xl pb-2">THIS IS ME</h1>
@@ -27,6 +27,38 @@ const Home = () => {
           <p className="text-gray-50 uppercase max-w-[95%] text-sm xl:text-md">Welcome to my own site. You will find here all the information about my previous work and models that I have implemented. You can also request the design of your site. You can also contact me through social networking links or access my contact page.</p>
         </div>
       </div>
+
+      <div className="grid grid-cols-3 gap-4 mt-10">
+
+        <div className="grid place-items-center bg-gray-700 border-2 border-gray-600 shadow-slate-800 shadow-2xl rounded-2xl p-2">
+          <h1 className="py-2">
+            <VisitorCounter />
+          </h1>
+          <div className="flex flex-row space-x-2 px-2">
+            <RemoveRedEye />
+            <span>visitors</span>
+          </div>
+        </div>
+
+        <div className="grid place-items-center bg-gray-700 border-2 border-gray-600 shadow-slate-800 shadow-2xl rounded-2xl p-2">
+          <h1 className="py-2">0</h1>
+          <div className="flex flex-row space-x-2 px-2">
+            <DomainVerification />
+            <span>Projects Completed</span>
+          </div>
+        </div>
+
+        <div className="grid place-items-center bg-gray-700 border-2 border-gray-600 shadow-slate-800 shadow-2xl rounded-2xl p-2">
+          <h1 className="py-2">0</h1>
+          <div className="flex flex-row space-x-2 px-2">
+            <InsertEmoticon />
+            <span>Happy Clients</span>
+          </div>
+        </div>
+
+      </div>
+
+
 
       <div className="grid place-items-center w-full my-4">
         <h1 className="text-center text-3xl text-gray-50 uppercase mt-10">My Offered Services</h1>
@@ -63,27 +95,7 @@ const Home = () => {
       </div>
 
 
-      <div className="flex flex-row justify-items-center space-x-4 mt-4">
 
-        <div className="grid place-items-center bg-gray-700 border-2 border-gray-600 shadow-slate-800 shadow-2xl rounded-2xl p-2">
-          <h1 className="py-2">
-            <VisitorCounter />
-          </h1>
-          <div className="flex flex-row space-x-2 px-2">
-            <RemoveRedEye />
-            <span>visitors</span>
-          </div>
-        </div>
-
-        <div className="grid place-items-center bg-gray-700 border-2 border-gray-600 shadow-slate-800 shadow-2xl rounded-2xl p-2">
-          <h1 className="py-2">0</h1>
-          <div className="flex flex-row space-x-2 px-2">
-            <DomainVerification />
-            <span>Projects Completed</span>
-          </div>
-        </div>
-
-      </div>
 
 
 
